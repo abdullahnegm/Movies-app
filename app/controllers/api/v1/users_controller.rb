87ed8d757bd1539f0d3225d6afd08f2395dad6da
abdullah <abdullah@lifetimely.io>
2024-05-20 20:26:16 +0300

    skip authentication for users test api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UsersController < Api::V1::ApplicationController
+      skip_before_action :authenticate_request, only: [:index] # This API is for testing, to get a username and the password is fixed anyway
 
       def index
         render_success('movies', User.all.map { |user| Users::IndexPresenter.new(user).as_json }, :ok)
