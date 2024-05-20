f56524d5a5c8e6697895af8ac8c8e32b78b6a188
abdullah <abdullah@lifetimely.io>
2024-05-20 22:33:30 +0300

    create pagination methods

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,13 +6,25 @@ module Api
       include ResponseHandler
       
       # skip_before_action :verify_authenticity_token
-      before_action :authenticate_request
+      # before_action :authenticate_request
 
       def authenticate_request
         @current_user = AuthorizeApiRequest.new(headers: request.headers).call
         return render_error('Token expired, Authorization failed!', :unauthorized) if @current_user.nil?
     
         @current_user
+      end
+
+      def search_text
+        params[:search] || ""
+      end
+
+      def limit
+        params[:limit] || 10
+      end
+
+      def offset
+        params[:offset] || 0
       end
 
     end
