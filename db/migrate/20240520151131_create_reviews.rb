9fb5e7a14a0e1253ecc3d507003a5dd2dc51350d
abdullah <abdullah@lifetimely.io>
2024-05-20 22:37:23 +0300

    create ratings column for reviews

--- db/migrate/20240520151131_create_reviews.rb
+++ db/migrate/20240520151131_create_reviews.rb
@@ -3,12 +3,11 @@ class CreateReviews < ActiveRecord::Migration[7.1]
     create_table :reviews do |t|
       t.bigint :movie_id, null: false
       t.bigint :user_id
-      t.bigint :actor_id, null: false
+      t.integer :rating, null: false
       t.string :review, null: false
 
       t.index :movie_id
       t.index :user_id
-      t.index :actor_id
 
       t.timestamps
     end
