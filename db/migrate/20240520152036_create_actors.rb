6e8f82cf2220afe7b044f885dd789077546c1ea7
abdullah <abdullah@lifetimely.io>
2024-05-20 18:50:38 +0300

    Create actors migration file

---
+++ db/migrate/20240520152036_create_actors.rb
@@ -0,0 +1,11 @@
+class CreateActors < ActiveRecord::Migration[7.1]
+  def change
+    create_table :actors do |t|
+      t.string :name
+
+      t.index :name
+
+      t.timestamps
+    end
+  end
+end
