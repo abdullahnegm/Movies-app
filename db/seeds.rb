f11ca13bbbfaa70e8e084066b5a4a43ae2ef53f7
abdullah <abdullah@lifetimely.io>
2024-05-20 22:37:40 +0300

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,60 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Movie.delete_all
+User.delete_all
+Review.delete_all
+MovieReview.delete_all
+
+
+
+100.times do
+  Movie.create!(
+    title: Faker::Movie.title,
+    description: Faker::Lorem.paragraph(sentence_count: 5),
+    year: Faker::Number.between(from: 1900, to: 2024),
+    director: Faker::Name.name,
+    filming_location: Faker::Address.city,
+    country: Faker::Address.country,
+    actors: Array.new(3) { Faker::Name.name }
+  )
+end
+
+puts "Created 100 movies"
+
+
+50.times do
+  User.create!(
+    username: Faker::Internet.username,
+    password: "123", # So its easier to test 
+    name: Faker::Name.name,
+    email: Faker::Internet.email
+  )
+end
+
+puts "Created 50 users"
+
+
+300.times do
+  Review.create!(
+    movie_id: Movie.all.sample.id,
+    user_id: User.all.sample.id,
+    rating: rand(1..5).to_i,
+    review: Faker::Lorem.sentence(word_count: 10)
+  )
+end
+
+puts "Created 50 reviews"
+
+
+Review.all.each do |review|
+  MovieReview.create!(
+    movie_id: Movie.all.sample.id,
+    review_id: review.id
+  )
+end
+
+puts "Created 50 Movie Reviews"
+
